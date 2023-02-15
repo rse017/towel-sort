f45a37abbc9746ef4ebcf31d4e5e4c02ff45a329
Sergei R <rse017@gmail.com>
2023-02-15 23:31:24 +0300

    feat: test solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 module.exports = function towelSort (matrix) {
-const a = [];
+    if (matrix == undefined){
+        return [];
+      }
+  
+const arr = [];
     for (let i = 0; i < matrix.length; i++) {
         if (i % 2 == 0) {
-            a.push(...matrix[i]);
+            arr.push(...matrix[i]);
         } 
         else {
-            a.push(...matrix[i].reverse());
+            arr.push(...matrix[i].reverse());
         }
     }
-    return a;
+    return arr;
 }
+  
+ /* Другой вариант
+module.exports = function towelSort (matrix=[]) {
+   return [].concat(...matrix.map((arr,index)=> index % 2 == 1 ? arr.reverse() : arr));
+}*/
+
+
+
